75a4c94c74fb60e67a812fef9dae36fda0797125
PierLuisVona <pg.vona@gmail.com>
2019-01-18 18:03:53 +0100

    sketch

---
+++ MaxMatrix.cpp
@@ -0,0 +1,137 @@
+/*
+ * MaxMatrix
+ * Version 1.0 Feb 2013
+ * Copyright 2013 Oscar Kin-Chung Au
+ */
+
+
+#include "Arduino.h"
+#include "MaxMatrix.h"
+
+MaxMatrix::MaxMatrix(byte _data, byte _load, byte _clock, byte _num) 
+{
+	data = _data;
+	load = _load;
+	clock = _clock;
+	num = _num;
+	for (int i=0; i<80; i++)
+		buffer[i] = 0;
+}
+
+void MaxMatrix::init()
+{
+	pinMode(data,  OUTPUT);
+	pinMode(clock, OUTPUT);
+	pinMode(load,  OUTPUT);
+	digitalWrite(clock, HIGH); 
+
+	setCommand(max7219_reg_scanLimit, 0x07);      
+	setCommand(max7219_reg_decodeMode, 0x00);  // using an led matrix (not digits)
+	setCommand(max7219_reg_shutdown, 0x01);    // not in shutdown mode
+	setCommand(max7219_reg_displayTest, 0x00); // no display test
+	
+	// empty registers, turn all LEDs off
+	clearMatrix();
+	
+	setIntensity(0x0f);    // the first 0x0f is the value you can set
+}
+
+void MaxMatrix::setIntensity(byte intensity)
+{
+	setCommand(max7219_reg_intensity, intensity);
+}
+
+void MaxMatrix::clearMatrix()
+{
+	for (int i=0; i<8; i++) 
+		setColumnAll(i,0);
+		
+	for (int i=0; i<80; i++)
+		buffer[i] = 0;
+}
+
+void MaxMatrix::setCommand(byte command, byte value)
+{
+	digitalWrite(load, LOW);    
+	for (int i=0; i<num; i++) 
+	{
+		shiftOut(data, clock, MSBFIRST, command);
+		shiftOut(data, clock, MSBFIRST, value);
+	}
+	digitalWrite(load, LOW);
+	digitalWrite(load, HIGH);
+}
+
+
+void MaxMatrix::setColumn(byte col, byte value)
+{
+	int n = col / 8;
+	int c = col % 8;
+	digitalWrite(load, LOW);    
+	for (int i=0; i<num; i++) 
+	{
+		if (i == n)
+		{
+			shiftOut(data, clock, MSBFIRST, c + 1);
+			shiftOut(data, clock, MSBFIRST, value);
+		}
+		else
+		{
+			shiftOut(data, clock, MSBFIRST, 0);
+			shiftOut(data, clock, MSBFIRST, 0);
+		}
+	}
+	digitalWrite(load, LOW);
+	digitalWrite(load, HIGH);
+	
+	buffer[col] = value;
+}
+
+void MaxMatrix::setColumnAll(byte col, byte value)
+{
+	digitalWrite(load, LOW);    
+	for (int i=0; i<num; i++) 
+	{
+		shiftOut(data, clock, MSBFIRST, col + 1);
+		shiftOut(data, clock, MSBFIRST, value);
+		buffer[col * i] = value;
+	}
+	digitalWrite(load, LOW);
+	digitalWrite(load, HIGH);
+}
+
+void MaxMatrix::setDot(byte col, byte row, byte value)
+{
+    bitWrite(buffer[col], row, value);
+
+	int n = col / 8;
+	int c = col % 8;
+	digitalWrite(load, LOW);    
+	for (int i=0; i<num; i++) 
+	{
+		if (i == n)
+		{
+			shiftOut(data, clock, MSBFIRST, c + 1);
+			shiftOut(data, clock, MSBFIRST, buffer[col]);
+		}
+		else
+		{
+			shiftOut(data, clock, MSBFIRST, 0);
+			shiftOut(data, clock, MSBFIRST, 0);
+		}
+	}
+	digitalWrite(load, LOW);
+	digitalWrite(load, HIGH);
+}
+
+void MaxMatrix::writeFull(unsigned long value) {
+	for (int y=4; y>=0;y--){
+            for (int x=5; x>=0; x--){
+                int xs = 10-x;
+                int ys=  5-y;
+                int COL= (xs) % 8;
+                int ROW = ( xs<8 ? 15-ys : 7-ys);
+                setDot(ROW,COL,(1L & (value>>y*6+x)));
+                }
+            }
+}
---
+++ MaxMatrix.ino
@@ -0,0 +1,34 @@
+#include "MaxMatrix.h"
+#define NumMatrix 2       // Number of Matrices
+byte Buf7219[7]; // "width,height,data[5]" single character buffer.
+
+MaxMatrix lc=MaxMatrix(12,10,11, 2);  
+#define zero_code                  0b00001100010010010010010010001100
+#define one_code                   0b00000100001100000100000100001110
+#define two_code                   0b00001100010010000100001000011110
+#define three_code                 0b00001100010010000100010010001100
+#define smile_code                 0b00000000100001010010001100000000
+
+void setup()
+   {
+      for (int i=0; i< NumMatrix ; i++)
+        {
+           lc.init();
+           lc.setIntensity(1);
+        } 
+   }
+
+void loop()
+   {
+  
+    lc.writeFull(smile_code);
+    delay(1000);
+    lc.writeFull(three_code);
+ delay(1000);
+    lc.writeFull(two_code);
+  delay(1000);
+    lc.writeFull(one_code);
+  delay(1000);
+    lc.writeFull(zero_code);  
+  delay(1000);  
+ }
